2de4e2263e08c767b872ac36d3834e6ea81eadda
DanielEnriquezE <dany14555@outlook.com>
2021-05-26 11:50:08 -0500

    Verificar si se crearon las carpetas y ficheros

---
+++ CapaLogicaNegocio/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Admin
+{
+	public Admin()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
